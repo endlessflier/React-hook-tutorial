358a32fc2d943e212c710f38771682efba51bd92
Full Stack Developer <webdeveloper2009@outlook.com>
2022-03-10 04:39:33 -0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,83 @@
+import { useState, useRef, useReducer } from "react";
+import CallBackTutorial from "./CallbackTutorial";
+import MemoTutorial from "./MemoTutorial";
+import ContextTutorial from "./UseContext/ContextTutorial";
+import EffectTutorial from "./EffectTutorial";
+import "./styles.css";
+
+const StateTutorial = () => {
+  const [inputValue, setInputValue] = useState("Pedro");
+
+  const onChange = (event) => {
+    const newValue = event.target.value;
+    setInputValue(newValue);
+  };
+
+  return (
+    <div>
+      <input placeholder="enter something..." onChange={onChange} />
+      {inputValue}
+    </div>
+  );
+};
+
+function RefTutorial() {
+  const inputRef = useRef(null);
+
+  const onClick = () => {
+    inputRef.current.value = "";
+  };
+  return (
+    <div>
+      <h1>Pedro</h1>
+      <input type="text" placeholder="Ex..." ref={inputRef} />
+      <button onClick={onClick}>Change Name</button>
+    </div>
+  );
+}
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "INCREMENT":
+      return { count: state.count + 1, showText: state.showText };
+    case "toggleShowText":
+      return { count: state.count, showText: !state.showText };
+    default:
+      return state;
+  }
+};
+
+const ReducerTutorial = () => {
+  const [state, dispatch] = useReducer(reducer, { count: 0, showText: true });
+
+  return (
+    <div>
+      <h1>{state.count}</h1>
+      <button
+        onClick={() => {
+          dispatch({ type: "INCREMENT" });
+          dispatch({ type: "toggleShowText" });
+        }}
+      >
+        Click Here
+      </button>
+
+      {state.showText && <p>This is a text</p>}
+    </div>
+  );
+};
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+      <StateTutorial />
+      <RefTutorial />
+      <ReducerTutorial />
+      <MemoTutorial />
+      <CallBackTutorial />
+      <ContextTutorial />
+      <EffectTutorial />
+    </div>
+  );
+}
